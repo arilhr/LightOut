a8dc3e03dbb1c0a88c84fe7f0424e9bda0f7778f
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2021-05-24 01:52:17 +0700

    Add MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private GameObject tutorialPanel;
+    [SerializeField] private GameObject creditPanel;
+
+    public void Play(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Setting()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void Tutorial()
+    {
+        tutorialPanel.SetActive(true);
+    }
+
+    public void Credit()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        if (settingPanel.activeSelf == true)
+        {
+            settingPanel.SetActive(false);
+        }
+
+        if(tutorialPanel.activeSelf == true)
+        {
+            tutorialPanel.SetActive(false);
+        }
+
+        if(creditPanel.activeSelf == true)
+        {
+            creditPanel.SetActive(false);
+        }
+    }
+}
